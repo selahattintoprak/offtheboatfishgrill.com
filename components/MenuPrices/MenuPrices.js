835a64b45955a6eb6aa3fbb22e08301c2bcc60f4
selahattintoprak <selahattin47usa@hotmail.com>
2020-05-05 15:19:08 -0700

    added slideshow and menuPrices

---
+++ components/MenuPrices/MenuPrices.js
@@ -0,0 +1,55 @@
+export default ({ id, menuPrices }) => (
+  <>
+    <amp-accordion id={id} className="amp-accordion-container" disable-session-states="" animate="">
+      {menuPrices.map(({ title, subTitle, items, footer, link }, key) => (
+        <section className="amp-accordion" key={id + key}>
+          <h2 className="amp-accordion-header" style={{ fontSize: "1rem", padding: "0.3rem" }}>
+            {title}
+            <i class="icon icon-arrow-right"></i>
+          </h2>
+          <div className="amp-accordion-body my-2">
+            <div class="container" key={key}>
+              {subTitle}
+              {items.map(({ title, subTitle, price }, key) => (
+                <div class="columns" style={{ alignItems: "center" }}>
+                  <div class="column col-9">
+                    <strong>{title}</strong> <br></br>
+                    <span style={{ color: "rgb(94, 94, 94)" , fontWeight:"400"}}>{subTitle}</span>
+                  </div>
+                  <div class="column col-3 text-center">
+                    <strong>{price}</strong>
+                  </div>
+                </div>
+              ))}
+              {footer}
+            </div>
+          </div>
+        </section>
+      ))}
+    </amp-accordion>
+    <style jsx global>
+      {`
+        .amp-accordion {
+        }
+        .amp-accordion-header {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 16px;
+          background-color: white;
+          outline: none;
+        }
+        .amp-accordion-body {
+        }
+        .amp-accordion .amp-accordion-header .icon {
+          float: right;
+          transform: rotate(-90deg);
+          transition: transform 0.25s;
+        }
+        .amp-accordion[expanded] .amp-accordion-header .icon {
+          transform: rotate(90deg);
+        }
+      `}
+    </style>
+  </>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import Quotes from "../components/Quotes/Quotes";
 import SinglePage from "../components/SinglePage/SinglePage";
+import MenuPrices from "../components/MenuPrices/MenuPrices";
 
 export const config = { amp: true };
 
 export default () => (
   <>
     <SinglePage title="Greg's Truck Repair" description="Greg's Truck Repair">
-      <section className="section section-quotes" style={{ background: "#3dc2ff" }}>
+      <section className="section section-slideshow">
         <div className="container">
           <div className="columns">
-            <div className="column col-md-12 col-5 col-mx-auto">
+            <div className="column col-md-12 col-12 col-mx-auto col-my-auto">
               <h1
                 className="text-center"
                 style={{
@@ -19,11 +20,18 @@ export default () => (
                   marginTop: "4rem",
                 }}
               >
-                Book your move online
+                GREG'S TRUCK REPAIR IS OPEN ! ESSENTIAL BUSINESS MAINTAINING SUPPLY CHAIN
               </h1>
-
-              <div className="form-group quotes-form">
-                <Quotes extras></Quotes>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section section-menu-prices">
+        <div className="container">
+          <div className="columns">
+            <div className="column col-md-12 col-10 col-mx-auto">
+              <div className="">
+                <MenuPrices id="menu-prices" menuPrices={menuPrices} />
               </div>
             </div>
           </div>
@@ -45,11 +53,79 @@ export default () => (
           background: #ffffff;
           margin-bottom: 4rem;
         }
-        .section-quotes {
+        .section-slideshow {
+          display: flex;
           justify-content: center;
           align-items: center;
+          background-image: url("/static/images/slideshow.jpg");
+          min-height: 400px; /* You must set a specified height */
+          background-position: center; /* Center the image */
+          background-repeat: no-repeat; /* Do not repeat the image */
+          background-size: cover; /* Resize the background image to cover the entire container */
         }
       `}
     </style>
   </>
 );
+const menuPrices = [
+  {
+    title: "Garage Labor Rates",
+    subTitle: "Worked performed on site",
+    items: [
+      { title: "All Classes of Trucks, Trailers", subTitle: "", price: "$115/hr" },
+      { title: "Buses and RVs", subTitle: "", price: "$135/hr" },
+    ],
+    footer:
+      "Additional charges for those using third party maintenance vendors such as ARI, Element, GE Capital, ect..",
+  },
+  {
+    title: "Emergency Roadside Service",
+    subTitle: "Work performed roadside or at your location",
+    items: [
+      { title: "Service Truck Fee", subTitle: "Applied to all service calls", price: "$70" },
+      {
+        title: "Emergency Road Service Labor Rate",
+        subTitle: "Weekends and Holidays 3 Hour Minimum",
+        price: "$145/hr",
+      },
+    ],
+    footer: (
+      <>
+        <h4 style={{ color: "red" }} className="text-center">**24 / 7 / 365 Available for our Fleet Clients**</h4> Our Fleet clients can rest
+        assured we have their backs at all times, any time of day, any day of the year including weekends and Holidays!
+        (Above Rates Apply)
+      </>
+    ),
+  },
+  {
+    title: "Inspections",
+    subTitle: "Pricing does not include repairs if required",
+    items: [
+      { title: "Federal DOT Inspection", subTitle: "", price: "$115" },
+      { title: "Missouri State Safety Inspection", subTitle: "", price: "$12" },
+      { title: "GVIP Emissions Test", subTitle: "", price: "$24" },
+      { title: "IDOD Inspection", subTitle: "", price: "$1O" },
+      { title: "Pre-Buy Inspection", subTitle: "", price: "$115" },
+    ],
+  },
+  {
+    title: "",
+    subTitle: "",
+    items: [{ title: "", subTitle: "", price: "" }],
+  },
+  {
+    title: "",
+    subTitle: "",
+    items: [{ title: "", subTitle: "", price: "" }],
+  },
+  {
+    title: "",
+    subTitle: "",
+    items: [{ title: "", subTitle: "", price: "" }],
+  },
+  {
+    title: "",
+    subTitle: "",
+    items: [{ title: "", subTitle: "", price: "" }],
+  },
+];
