a6e3e14881cacfd8921830f4113b1fd02539c682
selahattintoprak <selahattin47usa@hotmail.com>
2020-05-13 18:48:02 -0700

    changed cols

--- components/Copyright/Copyright.js
+++ components/Copyright/Copyright.js
@@ -1,8 +1,8 @@
 export default ({ year, name }) => (
   <div className="container">
     <div className="columns">
-      <div className="column col-md-12 col-10 col-mx-auto">
-        <div className="">
+      <div className="column col-md-12 col-8 col-mx-auto">
+        <div className="text-center mb-2">
           Copyright © {year} {name} - All Rights Reserved.
         </div>
       </div>
