986791c2c3dbdbbd189fabd1968c3f04f05b06ed
selahattintoprak <selahattin47usa@hotmail.com>
2020-05-29 02:30:03 -0700

    added more menu schema

---
+++ client/src/data/schema/food-menu/sample.js
@@ -0,0 +1,61 @@
+export default {
+  "@type": "MenuSection",
+  name: "",
+  description: "",
+  //   image: "https://thisisarestaurant.com/starter_dishes.jpg",
+  offers: {
+    "@type": "Offer",
+    availabilityEnds: "T8:21:00",
+    availabilityStarts: "T8:11:00",
+  },
+  hasMenuItem: [
+    {
+      "@type": "MenuItem",
+      name: "Banana Banoffi",
+      description: "",
+      offers: {
+        "@type": "Offer",
+        price: "5.95",
+        priceCurrency: "USD",
+      },
+      // suitableForDiet: "http://schema.org/GlutenFreeDiet",
+    },
+  ],
+};
+const nested = {
+  "@type": "MenuSection",
+  name: "",
+  description: "",
+  //   image: "https://thisisarestaurant.com/starter_dishes.jpg",
+  offers: {
+    "@type": "Offer",
+    availabilityEnds: "T8:21:00",
+    availabilityStarts: "T8:11:00",
+    hasMenuSection: [
+      {
+        "@type": "MenuSection",
+        name: "",
+        description: "",
+        //   image: "https://thisisarestaurant.com/starter_dishes.jpg",
+        offers: {
+          "@type": "Offer",
+          availabilityEnds: "T8:21:00",
+          availabilityStarts: "T8:11:00",
+        },
+        hasMenuItem: [
+          {
+            "@type": "MenuItem",
+            name: "Banana Banoffi",
+            description: "",
+            offers: {
+              "@type": "Offer",
+              price: "5.95",
+              priceCurrency: "USD",
+            },
+            // suitableForDiet: "http://schema.org/GlutenFreeDiet",
+          },
+        ],
+      },
+    ],
+  },
+};
